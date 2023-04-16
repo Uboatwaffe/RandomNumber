40ec90ed6b16fb76885e1c3dabd69cb744fd1f1e
Maciej <mpiatek1512@gmail.com>
2023-04-16 21:47:50 +0200

    FINAL

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // ----!!!THIS IS FINAL VERSION!!!----
 
         // Objects
         Scanner sc = new Scanner(System.in);
